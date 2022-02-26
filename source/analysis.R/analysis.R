ca33f6fa959307768cd836bf7c9ede96d215f3ec
Tacy Chan <tacychan@Tacys-MacBook-Pro.local>
2022-02-25 18:20:16 -0800

    A3

--- source/analysis.R/analysis.R
+++ source/analysis.R/analysis.R
@@ -1,34 +1,58 @@
 
 
 #loading pacakges 
+install.packages("tidyverse")
+install.packages("dplyr")
 library("dplyr")
 library("tidyr")
 library("ggplot2")
-
+library("tidyverse")
 
 
 County <- ("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
-County_level_cvs <- read.csv(County, header = TRUE)
-View(County_level_cvs)
-
-Juristic <- ("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends_jail_jurisdiction.csv")
-Juristic_level_cvs <- read.csv(Juristic, header = TRUE)
-View(Juristic_level_cvs)
-
-# Variable 1 : Total Number of Black Male Incarnations county level. 
+County_level_csv <- read.csv(County, header = TRUE)
+View(County_level_csv)
 
 
+# Variable 1 : Total Number of Black Male Incarceration county level. 
+total_black_male <- County_level_csv %>%
+  filter(black_jail_pop == max(black_jail_pop,na.rm = TRUE)) %>%
+  pull(black_jail_pop)
 
-#Variable 2 : Total Number of White Male Incarnations 
+#Variable 2 : Total Number of White Male Incarceration
+total_white_male <- County_level_csv %>%
+  filter(white_jail_pop == max(white_jail_pop,na.rm = TRUE)) %>%
+  pull(white_jail_pop)
+ 
+#Variable 3: Total Number of Female Incarceration
+total_female_pop <- County_level_csv %>%
+  filter(female_jail_pop == max(female_jail_pop,na.rm = TRUE)) %>%
+  pull(female_jail_pop)
 
-#Variable 3: Total Number of Female Incarnations 
+#Variable 4 : State with highest Black Incarceration 
+highest_black_pop <- County_level_csv %>%
+  drop_na()%>%
+  filter(black_jail_pop == max(black_jail_pop))%>%
+  pull(state)
 
-#Variable 4 :Total Number of Native Incarnations in the South
+#Sub variable: State with the highest White Incarceration
+highest_white_pop <- County_level_csv %>%
+  drop_na()%>%
+  filter(white_jail_pop == max(white_jail_pop))%>%
+  pull(state)
 
-#Variable 5 Total Number of Hispanic Male Incarnations
+#Variable 5: Total Number of Black Male Incarceration in TX State 
+black_incarceration_tx <- County_level_csv %>%
+  drop_na() %>%
+  filter(state =="TX")%>%
+  filter(black_jail_pop == max(black_jail_pop))%>%
+  pull(black_jail_pop)
 
 
-#Variable Comparison  : 
+  #Sub Variable: Total Number of Male Incarceration in TX State 
+white_incarceration_tx <- County_level_csv %>%
+  drop_na() %>%
+  filter(state =="TX")%>%
+  filter(white_jail_pop == max(white_jail_pop))%>%
+  pull(white_jail_pop)
 
-#Map 
-#I chose to compare black males population in
